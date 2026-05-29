91826e41b3c88caab2c3730fbc667f26b0289246
smthemex <138738845+smthemex@users.noreply.github.com>
2026-05-29 19:19:43 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from comfy_api.latest import ComfyExtension, io
 from typing_extensions import override
 
-from .LongCat_Video_node import LongCat_Video_SM_Model,  LongCat_Video_SM_Sampler,LongCat_Video_SM_Encode,LongCat_Video_SM_Audio,LongCat_Video_SM_Vocal,LongCat_Video_SM_VocalModel
+from .LongCat_Video_node import LongCat_Video_SM_Model, LongCat_Video_SM_WhisperModel, LongCat_Video_SM_Sampler,LongCat_Video_SM_Encode,LongCat_Video_SM_Audio,LongCat_Video_SM_Vocal,LongCat_Video_SM_VocalModel
 
 class LongCat_Video_SM_Extension(ComfyExtension):
     @override
@@ -12,7 +12,8 @@ class LongCat_Video_SM_Extension(ComfyExtension):
             LongCat_Video_SM_Encode,
             LongCat_Video_SM_Audio,
             LongCat_Video_SM_Vocal,
-            LongCat_Video_SM_VocalModel
+            LongCat_Video_SM_VocalModel,
+            LongCat_Video_SM_WhisperModel,
         ]   
 
 async def comfy_entrypoint() -> LongCat_Video_SM_Extension:  # ComfyUI calls this to load your extension and its nodes.
